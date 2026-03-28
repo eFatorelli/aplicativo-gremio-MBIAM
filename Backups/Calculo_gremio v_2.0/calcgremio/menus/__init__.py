ce602fb2ddd37caff36d18b694783c41ee37614a
eFatorelli <enzofator@gmail.com>
2026-03-28 15:35:22 -0300

    Pasta Backup apagada (ver commits anteriores para recuperá-la)

--- Backups/Calculo_gremio v_2.0/calcgremio/menus/__init__.py
+++
@@ -1,8 +0,0 @@
-from .menu_calc import menu_calc
-from .menu_definicoes import menu_definicoes
-from .menu_inicial import menu_inicial
-from .menu_msg import menu_msg
-from .menu_pesquisa import menu_pesquisa
-from .menu_usuario import menu_usuario
-
-__all__ = ['menu_usuario','menu_definicoes','menu_inicial','menu_pesquisa','menu_calc']
